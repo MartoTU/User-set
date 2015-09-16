505dffa0dc21f833e30d6caab87eeece5884d1b4
Martin <martin.ruzhinov@gmail.com>
2015-09-17 02:25:53 +0300

    Choose number and text for output
    Simple basic program for choose the output numbers and text

---
+++ Userpick.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int number;
+	double decimal;
+	string text;
+cout<<"Please enter a number:";cin>>number;
+cout<<"Please enter a decimal number:";cin>>decimal;
+cout<<"Please enter a text:";cin>>text;
+
+cout<<number<<endl;
+cout<<decimal<<endl;
+cout<<text<<endl;
+
+return 0;
+
+
+	
+}
